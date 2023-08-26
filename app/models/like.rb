7dbb785cb3d083c910fbddf63612116fd855f841
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-26 12:34:01 +0600

    Fix linters errors

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,14 +1,14 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post , class_name: 'Post'
+  belongs_to :post, class_name: 'Post'
 
   validates :author, presence: true
   validates :post, presence: true
 
   after_create :increase_likes_counter
-  
+
   def increase_likes_counter
-    self.post.likes_counter += 1
-    self.post.save
+    post.likes_counter += 1
+    post.save
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :author , class_name: "User" , foreign_key: :author_id , dependent: :destroy
-  has_many :likes, dependent: :destroy 
+  belongs_to :author, class_name: 'User', foreign_key: :author_id, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :coments, dependent: :destroy
 
   validates :title, presence: true
@@ -11,11 +11,11 @@ class Post < ApplicationRecord
   after_create :increase_posts_counter
 
   def recent_comments
-    self.coments.order(created_at: :desc).limit(5)
+    coments.order(created_at: :desc).limit(5)
   end
 
   def increase_posts_counter
     self.post_counter += 1
-    self.save
+    save
   end
 end
