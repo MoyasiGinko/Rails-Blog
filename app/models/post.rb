b2365202d1647870bc0dbe2351ab25a757f91e02
Moyasi Ginko <mrsiyam61@gmail.com>
2023-08-27 09:34:01 +0600

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,18 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy # Fix the typo here
+  has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 100 }
+  validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :increment_post_counter
   before_destroy :decrement_post_counter
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5) # Use the correct association name here
+    comments.order(created_at: :desc).limit(5)
   end
 
   def increment_post_counter
