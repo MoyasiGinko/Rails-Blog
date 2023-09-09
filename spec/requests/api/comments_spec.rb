1867ec7d29a9183b1e7eca81875aff3a430a4904
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-10 03:58:17 +0600

    Update file

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -2,49 +2,39 @@ require 'swagger_helper'
 
 RSpec.describe 'api/comments', type: :request do
   path '/api/users/{user_id}/posts/{post_id}/comments' do
+    # You'll want to customize the parameter types...
     parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
     parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
 
-    %i[get post].each do |http_method|
-      operation "#{http_method} comments" do
-        response(200, 'successful') do
-          let(:user_id) { '123' }
-          let(:post_id) { '123' }
+    get('list comments') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
 
-          after do |example|
-            example.metadata[:response][:content] = {
-              'application/json' => {
-                example: JSON.parse(response.body, symbolize_names: true)
-              }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
             }
-          end
-          run_test!
+          }
         end
+        run_test!
       end
     end
-  end
 
-  path '/api/users/{user_id}/posts/{post_id}/comments/{id}' do
-    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
-    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
-    parameter name: 'id', in: :path, type: :string, description: 'id'
+    post('create comment') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
 
-    %i[get delete].each do |http_method|
-      operation "#{http_method} comment" do
-        response(200, 'successful') do
-          let(:user_id) { '123' }
-          let(:post_id) { '123' }
-          let(:id) { '123' }
-
-          after do |example|
-            example.metadata[:response][:content] = {
-              'application/json' => {
-                example: JSON.parse(response.body, symbolize_names: true)
-              }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
             }
-          end
-          run_test!
+          }
         end
+        run_test!
       end
     end
   end
