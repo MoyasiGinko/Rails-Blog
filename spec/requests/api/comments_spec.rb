f5ab172cdcc673ddf94f995ef8d64363a3c7e413
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-10 03:40:40 +0600

    Update api doc

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -38,4 +38,44 @@ RSpec.describe 'api/comments', type: :request do
       end
     end
   end
+  path '/api/users/{user_id}/posts/{post_id}/comments/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'user_id', in: :path, type: :string, description: 'user_id'
+    parameter name: 'post_id', in: :path, type: :string, description: 'post_id'
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show comment') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete comment') do
+      response(200, 'successful') do
+        let(:user_id) { '123' }
+        let(:post_id) { '123' }
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
 end
